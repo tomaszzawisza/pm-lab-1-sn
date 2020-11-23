e146d7eb98e6e70b50bb7808f71a8efb37723dc3
Tomasz <tomasz.zawisza@student.wat.edu.pl>
2020-11-23 14:38:40 +0100

    Zakończono zadanie 2.3.2

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Odpowiedź:  Nagłe zmiany stanów światel, co powoduje gwałtowne hamowanie ki
 
 ## Zadanie 2-3-2:
 Pytanie:    *Czy możliwe jest sterowanie stanem niskim (LOW) diod LED?*  
-Odpowiedź:  ...
+Odpowiedź:  Tak jest możliwe, należy odpowiednio zdefiniować porty
 
 # Zadania rozszerzajace
 ## Zadanie 3-1:
--- Zadanie-2-3/src/main.cpp
+++ Zadanie-2-3/src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+uint8_t ledPin[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+uint8_t ledDelay = 65;
+uint8_t direction = 1;
+uint8_t currentLED = 0;
+unsigned long changeTime;
+
+void changeLED();
 
 void setup() {
-  // put your setup code here, to run once:
+  for (int i = 0; i < 10; i++) {
+    pinMode(ledPin[i], OUTPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int x = 0; x < 10; x++) {
+    changeLED();
+    changeTime = millis();
+  }
+}
+
+void changeLED(){
+  for (int x = 0; x < 10; x++){
+    digitalWrite(ledPin[x], LOW);
+  }
+  digitalWrite(ledPin[currentLED], HIGH);
+  currentLED += direction;
+  if (currentLED == 9) {
+    direction =-1;
+  }
+  if (currentLED == 0 ){
+    direction = 1;
+  }
 }
